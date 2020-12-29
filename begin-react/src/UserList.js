59e5bbecac98d616a96bd587ecf11bc1b6315cb3
kwangminini <rhkdals7362@gmail.com>
2020-12-30 02:14:41 +0900

    useRef && array function skill up

---
+++ begin-react/src/UserList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function User( { user, onRemove, onToggle } ){
+    const {username, email, id, active} = user;
+    
+    return (
+        <div>
+            <b style={{
+                color: active? 'green' : 'black',
+                cursor:'pointer'
+            }
+            }
+            onClick={()=>onToggle(id)}>{username}</b> <span>{email}</span>
+            <button onClick={()=>onRemove(id)}>삭제</button>
+        </div>
+    )
+}
+
+export default function UserList({users, onRemove, onToggle}){
+    return(
+        <div>
+            {users.map(
+                user => (<User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>)
+            )}
+        </div>
+    )
+}
