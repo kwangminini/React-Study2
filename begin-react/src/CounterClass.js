9ab5f529f59e8c457f1b44f0229af7e11bda50c1
kwangminini <rhkdals7362@gmail.com>
2021-01-03 23:37:53 +0900

    Create TodoList

--- begin-react/src/CounterClass.js
+++ begin-react/src/CounterClass.js
@@ -1,27 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Counter extends Component{
-    
-    constructor(props){
+class Counter extends Component {
+
+    constructor(props) {
         super(props);
         this.state = {
-            counter:0
+            counter: 0
         };
     }
     handleIncrease = () => {
         this.setState({
-            counter: this.state.counter+1
+            counter: this.state.counter + 1
         });
     }
     handleDecrease = () => {
         this.setState({
-            counter:this.state.counter-1
+            counter: this.state.counter - 1
         })
     }
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h1>{this.state.counter}</h1>
                 <button onClick={this.handleIncrease}>+1</button>
