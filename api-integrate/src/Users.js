faf3e75803982aceb5c86b61f59f88438dae8a20
kwangminini <rhkdals7362@gmail.com>
2021-01-08 00:05:23 +0900

    add react-async library

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,20 +1,24 @@
 import React, {useReducer, useEffect, useState} from 'react';
 import axios from 'axios'; 
-import useAsync from './useAsync';
+// import useAsync from './useAsync';
+// import userAsnc from 'react-async';
 import User from './User';
+import { useAsync} from 'react-async';
 async function getUsers(){
     const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
     return response.data;
 }
 
 function Users(){
-    const [state, refetch] = useAsync(getUsers, [], true);
+    // const [state, refetch] = useAsync(getUsers, [], true);
     const [userId, setUserId] = useState(null);
-    const {loading, data: users, error} = state;
-
-    if(loading) return <div>로딩중 ..</div>;
+    //const {loading, data: users, error} = state;
+    const { data:users, error,isLoading, reload, run} = useAsync({
+        deferFn: getUsers
+    })
+    if(isLoading) return <div>로딩중 ..</div>;
     if(error) return <div>에러가 발생</div>
-    if(!users) return <button onClick={refetch}>불러오기</button>;
+    if(!users) return <button onClick={run}>불러오기</button>;
     return(
         <>
         <ul>
@@ -24,7 +28,7 @@ function Users(){
                </li> 
             ))}
         </ul>
-        <button onClick={refetch}>다시 불러오기</button>
+        <button onClick={reload}>다시 불러오기</button>
         {userId && <User id = {userId}/>}
         </>
     );
