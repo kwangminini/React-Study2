bde12466d5c1e03b50879a3a6069f91511a75a9e
kwangminini <rhkdals7362@gmail.com>
2021-01-11 01:39:14 +0900

    create withRouter

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,16 +3,19 @@ import Home from './Home';
 import About from './About';
 import {Route, Link} from 'react-router-dom';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 function App() {
   return (
     <>
     <Link to="/">홈</Link>
     <Link to="/about">소개</Link>
     <Link to="/profiles">프로필 목록</Link>
+    <Link to="/history">예제</Link>
     <div>
       <Route exact path="/" component={Home}/>
       <Route path="/about" component={About}/>
       <Route path="/profiles" component={Profiles}/>
+      <Route path="/history" component={HistorySample}/>
     </div>
     </>
   );
