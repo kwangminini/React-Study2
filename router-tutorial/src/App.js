83670cea57dbcad86c2a04a332d309418b3a01c8
kwangminini <rhkdals7362@gmail.com>
2021-01-10 18:45:45 +0900

    create sub-router

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Home from './Home';
 import About from './About';
 import {Route, Link} from 'react-router-dom';
-import Profile from './Profile';
+import Profiles from './Profiles';
 function App() {
   return (
     <>
     <Link to="/">홈</Link>
     <Link to="/about">소개</Link>
+    <Link to="/profiles">프로필 목록</Link>
     <div>
       <Route exact path="/" component={Home}/>
       <Route path="/about" component={About}/>
-      <Route path="/profiles/:username" component={Profile}/>
+      <Route path="/profiles" component={Profiles}/>
     </div>
     </>
   );
---
+++ router-tutorial/src/Profiles.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link, Route} from 'react-router-dom';
+import Profile from './Profile';
+function Profiles(){
+    return(
+        <div>
+            <h1>사용자 목록</h1>
+            <ul>
+                <li><Link to="/profiles/velopert">velopert</Link></li>
+                <li><Link to="/profiles/home">homer</Link></li>
+            </ul>
+            <Route path="/profiles" exact render={()=> <div>사용자를 선택해 주세요</div>}/>
+            <Route path="/profiles/:username" component={Profile}/>
+            
+        </div>
+    );
+}
+
+export default Profiles;
