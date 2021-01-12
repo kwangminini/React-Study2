db81034dde42e2a27f17f2cc290bb2ce82cd3442
kwangminini <rhkdals7362@gmail.com>
2021-01-13 03:33:34 +0900

    add Connect HOC

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {connect} from 'react-redux';
 import { increase, setDiff, decrease } from '../modules/counter';
 import Counter from '../components/Counter';
 
-function CounterContainer(){
-    const {number, diff} = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff
-    }));
-
-    const dispatch = useDispatch();
-
-    const onIncrease = () => dispatch(increase());
-    const onDecrease = () => dispatch(decrease());
-    const onSetDiff = diff => dispatch(setDiff(diff));
+function CounterContainer({
+    number,diff,onIncrease,onDecrease,onSetDiff
+}){
     return <Counter
             number={number}
             diff={diff}
@@ -23,4 +15,16 @@ function CounterContainer(){
 
 }
 
-export default CounterContainer;
+const mapStateToProps = state => ({
+    number: state.counter.number,
+    diff: state.counter.diff  
+});
+
+const mapDispatchToProps = dispatch => ({
+    onIncrease: () => dispatch(increase()),
+    onDecrease: () => dispatch(decrease()),
+    onSetDiff: (diff) => dispatch(setDiff(diff))
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
