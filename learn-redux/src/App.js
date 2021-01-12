4e6f528de65145e282beef24561755e0d148cd66
kwangminini <rhkdals7362@gmail.com>
2021-01-13 02:24:36 +0900

    optimization

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -2,8 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import './exercise';
 import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 function App() {
-  return <CounterContainer/>;
+  return(
+    <div>
+      <CounterContainer/>
+      <TodosContainer/>
+    </div>
+  );
+  
 }
 
 export default App;
