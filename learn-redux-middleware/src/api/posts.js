881a6639f94b104abab1fc434318b149075171b1
kwangminini <rhkdals7362@gmail.com>
2021-01-17 15:36:28 +0900

    create redux-thunk

---
+++ learn-redux-middleware/src/api/posts.js
@@ -0,0 +1,29 @@
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+
+const posts = [
+    {
+        id:1,
+        title: '리덕스 미들웨어를 배워봅시다.',
+        body: '리덕스 미들웨어를 직접 만들어 봄'
+    },
+    {
+        id:2,
+        title: 'redux-thunk를 사용해보자',
+        body: 'redux-thunk를 사용해서 비동기 작업을 처리'
+    },
+    {
+        id:3,
+        title: 'redux-saga도 사용',
+        body: '나중에 redux-saga를 사용해서 비동기 작업을 처리'
+    },
+];
+
+export const getPosts = async() => {
+    await sleep(500);
+    return posts;
+}
+
+export const getPostsById = async(id) => {
+    await sleep(500);
+    return posts.find(post => post.id === id );
+}
