dcb81607d50e82ed8b827c1db9144a990a7add58
kwangminini <rhkdals7362@gmail.com>
2021-02-09 02:36:03 +0900

    typescript redux todoList tutorial

--- ts-react-redux-tutorial2/src/App.tsx
+++ ts-react-redux-tutorial2/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import TodoApp from './containers/TodoApp';
 
 function App() {
   return (
-    <CounterContainer/>
+    <TodoApp/>
   );
 }
 
